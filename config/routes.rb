a92707b1e01b4674d8fd8a21eaef5d36433afbc5
Josh Vince <joshmvince@gmail.com>
2023-06-01 17:19:19 +0100

    Allow ngrok and convert public scratch endpoint to get

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/scratches/:id', to: 'scratches#show', as: 'scratch'
   get '/scratches/add/:id', to: 'scratches#add', as: 'add_scratch'
   get '/scratches/remove/:id', to: 'scratches#remove', as: 'remove_scratch'
-  post '/add_scratch', to: 'scratches#add_to_today', as: 'add_scratch_to_today'
+  get '/add_scratch', to: 'scratches#add_to_today', as: 'add_scratch_to_today'
 
   get '/days/:id/ear_state', to: 'days#ear_state', as: 'ear_state'
   post '/days/:id/update_ear_state', to: 'days#update_ear_state', as: 'update_ear_state'
